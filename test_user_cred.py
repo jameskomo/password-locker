ca2ab67ea5afc06410057602ebae383e7079ccab
jameskomo <james.komoh@gmail.com>
2019-04-15 20:24:18 +0300

    Object initialization test case created and confirmed as failed. Function created to pass test

--- test_user_cred.py
+++ test_user_cred.py
@@ -16,7 +16,17 @@ class TestUsers(unittest.TestCase):
 		"""
 		self.new_user = Users("Simon","Gatheru","W3w3n!mkenya")
 		
+	def test_init(self):
+		"""
+		#test_init test case to test if the object is the initialized properly
+		"""
+		
+		self.assertEqual(self.new_user.first,"Simon")
+		self.assertEqual(self.new_user.last,"Gatheru")
+		self.assertEqual(self.new_user.password,"W3w3n!mkenya")
+		
 
+	
 	
 if __name__ == '__main__':
 	unittest.main()
